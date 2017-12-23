a96bd8fe8ff39724a3a7e656a990058b88954025
Oleksandr Kaleniuk <akalenuk@gmail.com>
2017-12-23 14:43:34 +0200

    add unused stuff finder in tools

---
+++ tools/find_unused_stuff_in_htmls/find_unused_stuff_in_htmls.py
@@ -0,0 +1,21 @@
+import os
+
+for file in os.listdir('.'):
+	if file.endswith('.html'):
+		styles = []
+		functions = []
+		f = open(file, 'r')
+		text = f.read()
+		styles = text.split('</style>')[0].split('<style>')[1]
+		styles = [line for line in styles.split('\n') if len(line) > 0]
+		styles = [style for style in styles if style[0] == '.']
+		styles = [style.split('{')[0].strip()[1:] for style in styles]
+		for style in styles:
+			if text.find('"'+style+'"') == -1 and text.find("'"+style+"'") == -1:
+				print 'style "' + style + '" is not used in ' + file
+		functions = text.split('<script language="JavaScript">')[1].split('</script>')[0]
+		functions = [fun.split('(')[0].strip() for fun in functions.split('function ')[1:]]
+		for fun in functions:
+			if text.count(fun) == 1:
+				print 'function "' + fun + '" is not used in ' + file
+
