be4bd22dfd49635f6ac77a01e2c3960cfe30600c
OKaleniuk <akalenuk@gmail.com>
2018-01-07 19:29:44 +0000

    another factorial

---
+++ drafts/static_c/factorial_2.s
@@ -0,0 +1,39 @@
+	.syntax unified
+	.arch armv7-a
+	.eabi_attribute 27, 3
+	.eabi_attribute 28, 1
+	.fpu vfpv3-d16
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 2
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.thumb
+	.file	"loop.c"
+	.section	.text.startup,"ax",%progbits
+	.align	2
+	.global	main
+	.thumb
+	.thumb_func
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	movw	r0, #:lower16:.LC0
+	movs	r1, #24
+	push	{r3, lr}
+	movt	r0, #:upper16:.LC0
+	bl	printf
+	movs	r0, #0
+	pop	{r3, pc}
+	.size	main, .-main
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"%d\012\000"
+	.ident	"GCC: (Debian 4.9.2-10) 4.9.2"
+	.section	.note.GNU-stack,"",%progbits
