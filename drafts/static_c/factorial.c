c6b3ae3521490d88b03ea1b6f8ea66183ebca091
OKaleniuk <akalenuk@gmail.com>
2018-01-07 19:34:33 +0000

    small fix

--- drafts/static_c/factorial.c
+++ drafts/static_c/factorial.c
@@ -1,10 +1,10 @@
 
-#define LOOP_0(ID, I, body) ID = I; (body);}
-#define LOOP_1(ID, I, body) ID = I; (body); LOOP_0(ID, I-1, body)
-#define LOOP_2(ID, I, body) ID = I; (body); LOOP_1(ID, I-1, body)
-#define LOOP_3(ID, I, body) ID = I; (body); LOOP_2(ID, I-1, body)
-#define LOOP_4(ID, I, body) ID = I; (body); LOOP_3(ID, I-1, body)
-#define LOOP_TO_0(ID, I, body) {int ID; LOOP_##I(ID, I, body) 
+#define LOOP_0(ID, body) ID = 0; (body);}
+#define LOOP_1(ID, body) ID = 1; (body); LOOP_0(ID, body)
+#define LOOP_2(ID, body) ID = 2; (body); LOOP_1(ID, body)
+#define LOOP_3(ID, body) ID = 3; (body); LOOP_2(ID, body)
+#define LOOP_4(ID, body) ID = 4; (body); LOOP_3(ID, body)
+#define LOOP_TO_0(ID, I, body) {int ID; LOOP_##I(ID, body) 
 
 #define FACTORIAL(X, Y) Y = 1; LOOP_TO_0(i, X , Y *= (i+(i==0)));
 
