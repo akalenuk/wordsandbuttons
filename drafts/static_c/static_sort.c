fe36c3864f6c5597536c91d25dc621aa47a677d2
OKaleniuk <akalenuk@gmail.com>
2018-01-08 07:40:01 +0000

    macros for static_sort_int

--- drafts/static_c/static_sort.c
+++ drafts/static_c/static_sort.c
@@ -6,19 +6,21 @@ case 1: ID=1;{BODY};\
 case 0: ID=0;{BODY};\
 }}
 
+#define STATIC_SORT_INT(a) \
+  LOOP_TO_0(i, sizeof(a)/sizeof(a[0]) - 2, LOOP_TO_0(j, i, \
+    int k = i-j; \
+    int s = a[k]+a[k+1]; \
+    int d = abs(a[k]-a[k+1]); \
+    a[k] = (s-d)/2; \
+    a[k+1] = (s+d)/2; \
+  ));
 
 #include "stdio.h"
 #include "stdlib.h"
 
 int main(){
   int a[4] = {1, 5, 2, 4};
-  LOOP_TO_0(i, sizeof(a)/sizeof(a[0]) - 2, LOOP_TO_0(j, i,
-    int k = i-j; 
-    int s = a[k]+a[k+1];
-    int d = abs(a[k]-a[k+1]);
-    a[k] = (s-d)/2;
-    a[k+1] = (s+d)/2;
-  ));
+  STATIC_SORT_INT(a);
   printf("%d %d %d %d\n", a[0], a[1], a[2], a[3]);
 }
 
