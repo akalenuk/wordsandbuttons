2ed64a8e51113ad03870f666e59d3a26853329c5
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-01-10 21:01:48 +0200

    solver in progres

--- drafts/static_c/loop.c
+++ drafts/static_c/loop.c
@@ -15,14 +15,32 @@ case 0: ID=0;{BODY};\
 #define LOOP_DOWNTO(ID, START, END, BODY) \
   LOOP_TO_0(ID, (START)-(END), ID=(END)+ID; BODY)
 
+#define SOLVE_A(A, B, I, J, N) \
+  ((N == sizeof(B)/sizeof(B[0])) ? A[I][I] : \
+    SOLVE_A(A, B, I, J, N+1) * SOLVE_A(A, B, N, N, N+1) - \
+    SOLVE_A(A, B, I, N, N+1) * SOLVE_A(A, B, N, J, N+1))
+
+#define SOLVE_B(A, B, I, N) \
+  ((N == sizeof(B)/sizeof(B[0])) ? B[I] : \
+    SOLVE_A(A, B, N, N, N+1) * SOLVE_B(A, B, I, N+1) - \
+    SOLVE_A(A, B, I, N, N+1) * SOLVE_B(A, B, N, N+1))
+
+#define SOLVE_X(A, B, X, I) \
+  double d = SOLVE_B(A, B, I, I+1); \
+  LOOP_TO(j, 0, I-1, d-=SOLVE_A(A, B, I, j, I+1)*X[j];); \
+  X[I] = d / SOLVE_A(A, B, I, I, I+1);
+
+#define STATIC_SOLVE(A, B, X) \
+  LOOP_TO(k, 0, sizeof(X)/sizeof(X[0])-1, SOLVE_X(A, B, X, k););
+
 #include "stdio.h"
 #include "stdlib.h"
 
 int main(){
-  printf("2 to 4: ");
-  LOOP_TO(i, 2, 4, printf("%d, ", i); );
-  printf("\n5 to 3: ");
-  LOOP_DOWNTO(j, 5, 3, printf("%d, ", j); );
-  printf("\n");
+  double a[3][3] = {{4., 2., 5.}, {1., 6., 4.}, {6.,2.,7.}};
+  double b[3] = {5., 3., 1.};
+  double x[3];
+  STATIC_SOLVE(a, b, x);
+  printf("%f %f %f", x[0], x[1], x[2]);
 }
 
