21db97ff391fbe3462f03e6e04e92259e7cc50b6
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-01-13 12:04:33 +0200

    typo

--- drafts/static_c/loop.c
+++ drafts/static_c/loop.c
@@ -1,41 +1,35 @@
-#define F_0(N) ((N)==1 ? 1 : (N)*F_1((N)-1))
-#define F_1(N) ((N)==1 ? 1 : (N)*F_2((N)-1))
-#define F_2(N) ((N)==1 ? 1 : (N)*F_3((N)-1))
-#define F_3(N) ((N)==1 ? 1 : (N)*F_4((N)-1))
-#define F_4(N) ((N)==1 ? 1 : (N)*F_5((N)-1))
-#define F_5(N) ((N)==1 ? 1 : (N)*F_6((N)-1))
-#define F_6(N) ((N)==1 ? 1 : (N)*F_7((N)-1))
-#define F_7(N) ((N)==1 ? 1 : (N)*F_8((N)-1))
-#define F_8(N)  (1)
+#incude "static_loops.h"
+#incude "static_recursion.h"
 
-inline int f_8(int i) {return 1;}
-inline int f_7(int i) {return i == 1 ? 1 : i*f_8(i-1);}
-inline int f_6(int i) {return i == 1 ? 1 : i*f_7(i-1);}
-inline int f_5(int i) {return i == 1 ? 1 : i*f_6(i-1);}
-inline int f_4(int i) {return i == 1 ? 1 : i*f_5(i-1);}
-inline int f_3(int i) {return i == 1 ? 1 : i*f_4(i-1);}
-inline int f_2(int i) {return i == 1 ? 1 : i*f_3(i-1);}
-inline int f_1(int i) {return i == 1 ? 1 : i*f_2(i-1);}
-inline int f_0(int i) {return i == 1 ? 1 : i*f_1(i-1);}
+RECURSIVE_4_CALLS(double aijn, (double* a, double* b, int i, int j, int n, int m){ 
+  return (n == m) ? a[i*m+i] : 
+    aijn, (a, b, i, j, n+1, m) * aijn, (a, b, n, n, n+1, m) - 
+    aijn, (a, b, i, n, n+1, m) * aijn, (a, b, n, j, n+1, m); 
+}, (double* a, double* b, int i, int j, int n, int m){return 0.})
 
-#define RECURSIVE(PREFIX, CHUNK1, SUFFIX, END_SUFFIX) \
-inline PREFIX##_8 END_SUFFIX \
-inline PREFIX##_7 CHUNK1##_8 SUFFIX \
-inline PREFIX##_6 CHUNK1##_7 SUFFIX \
-inline PREFIX##_5 CHUNK1##_6 SUFFIX \
-inline PREFIX##_4 CHUNK1##_5 SUFFIX \
-inline PREFIX##_3 CHUNK1##_4 SUFFIX \
-inline PREFIX##_2 CHUNK1##_3 SUFFIX \
-inline PREFIX##_1 CHUNK1##_2 SUFFIX \
-inline PREFIX##_0 CHUNK1##_1 SUFFIX 
+RECURSIVE_4_CALLS(double bin, (double* a, double* b, int i, int n, int m){ 
+  return (n == m) ? b[i] : 
+    aijn, (a, b, n, n, n+1, m) * bin, (a, b, i, n+1, m) - 
+    aijn, (a, b, i, n, n+1, m) * bin, (a, b, n, n+1, m);
+}, (double* a, double* b, int i, int n, int m){return 0.})
 
+#define SOLVE_X(A, B, X, I, M) \
+  double d = bin(A, B, I, (I+1), M); \
+  LOOP_TO(j, 0, I-1, d-=aijn(A, B, I, j, (I+1), M)*X[j];); \
+  X[I] = d / aijn(A, B, I, I, (I+1), M);
 
-RECURSIVE(int ff, (int i){return i == 1 ? 1 : i*ff, (i-1);},   (int i){return 0;})
+#define STATIC_SOLVE(A, B, X) \
+  int m = sizeof(X) / sizeof(X[0]); \
+  LOOP_TO(k, 0, m-1, SOLVE_X(&A[0][0], B, X, k, m););
 
 #include "stdio.h"
 #include "stdlib.h"
 
 int main(){
-  printf("%d", ff_0(4));  
+  double a[3][3] = {{4., 2., 5.}, {1., 6., 4.}, {6. ,2. ,7. }};
+  double b[3] = {5., 3., 1.};
+  double x[3];
+  STATIC_SOLVE(a, b, x);
+  printf("%f %f %f", x[0], x[1], x[2]);
 }
 
