40655a0b104046c6abcb64b6ee909183e98458fa
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-01-23 08:38:35 +0200

    deleted deferred

--- deferred/text_vs_api/files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def count_all_py_shell(path = '.'):
-    return os.popen('ls -R "' + path +'" | grep ".py$" | wc -l').read()
-
-def count_all_py(path = '.'):
-    return len([1 for (_, _, files) in os.walk(path) for f in files if f.endswith('.py')])
-    
-print count_all_py()
