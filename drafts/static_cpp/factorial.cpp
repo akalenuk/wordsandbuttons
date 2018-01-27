13679b59ae5e69f6816f58ab34348a541393bdfe
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-01-27 09:06:59 +0200

    factorial example

---
+++ drafts/static_cpp/factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template <int N>
+int factorial(){
+    return N == 1 
+        ? 1
+        : N*factorial<N - 1 + (N == 1)>();
+}
+
+int main(){
+    return factorial<4>();
+}
+
