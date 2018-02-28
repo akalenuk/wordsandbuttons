8db3fcd3b8ed6187fa3fc30a35d9cb675db7de73
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-02-28 07:16:03 +0200

    arrays experiment

--- drafts/python_to_llvm/test.c
+++ drafts/python_to_llvm/test.c
@@ -1,9 +1,9 @@
-double calculate(double a, double b){
-    return a / b;
+double calculate(double a, double* b){
+    return a / (b[0] + b[1]);
 }
 
 int main(){
     volatile double a = 2;
-    volatile double b = 3;
+    volatile double b[2] = {3, 5};
 	return (int)(calculate(a, b));
 }
