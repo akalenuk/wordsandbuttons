67b4b3c62abebd0d4becd4e1c2fe6f69c268c86e
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-05-30 18:27:18 +0300

    sizeof test

---
+++ drafts/double_error/sizeof_floats.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+  std::cout << "float: " << sizeof(float) << "\n";
+  std::cout << "double: " << sizeof(double) << "\n";
+  std::cout << "long double: " << sizeof(long double) << "\n";
+  return 0;
+}
