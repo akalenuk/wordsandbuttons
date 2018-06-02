93fc817568ac7bf1aaf5c0b74c2bfccc9b00aa13
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-06-02 17:33:57 +0300

    cubic solver to test

---
+++ drafts/double_error/cubic_solver.cpp
@@ -0,0 +1,38 @@
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <limits>
+
+const double PI = std::atan(1.) * 4.;
+
+std::array<double, 3> solve_cubic(std::array<double, 4> abcd) {
+    double a1 = abcd[1] / abcd[0];
+    double a2 = abcd[2] / abcd[0];
+    double a3 = abcd[3] / abcd[0];
+    double q = (a1 * a1 - 3. * a2) / 9.;
+    double sq = -2. * std::sqrt(q);
+    double r = (2. * a1 * a1 * a1 - 9. * a1 * a2 + 27. * a3) / 54.0;
+    double z = r * r - q * q * q;
+
+    std::array<double, 3> roots;
+    if(z <= 0) {
+        double t = std::acos(r / std::sqrt(q * q * q));
+        roots[0] = sq * std::cos(t / 3.) - a1 / 3.;
+        roots[1] = sq * std::cos((t + 2. * PI) / 3.) - a1 / 3.;
+        roots[2] = sq * std::cos((t + 4. * PI) / 3.) - a1 / 3.;
+    } else {
+        roots[0] = pow(std::sqrt(z) + std::abs(r) , 1. / 3.);
+        roots[0] += q / roots[0];
+        roots[0] *= ( r < 0 ) ? 1 : -1;
+        roots[0] -= a1 / 3.;
+        roots[1] = std::numeric_limits<double>::quiet_NaN();
+        roots[2] = std::numeric_limits<double>::quiet_NaN();
+    }
+    return roots;
+}
+
+int main() {
+    auto roots = solve_cubic({-4., -3., 2., 1.});
+    std::cout << roots[0] << " " << roots[1] << " " << roots[2] << "\n";
+}
+
