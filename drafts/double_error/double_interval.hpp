53899dd971df4f1475e6f2484f4279ac92a88ec9
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-06-04 20:22:36 +0300

    minimum for the interval

--- drafts/double_error/double_interval.hpp
+++ drafts/double_error/double_interval.hpp
@@ -11,9 +11,39 @@ struct DoubleInterval
     }
     DoubleInterval operator+(const DoubleInterval& that)
     {
-        from = this->from + that.from;
-        to = this->to + that.to;
+        from = static_cast<double>(this->from) + static_cast<double>(that.from);
+        to = static_cast<double>(this->to) + static_cast<double>(that.to);
         return *this;
+    }
+    DoubleInterval operator-(const DoubleInterval& that)
+    {
+        from = static_cast<double>(this->from) - static_cast<double>(that.to);
+        to = static_cast<double>(this->to) - static_cast<double>(that.from);
+        return *this;
+    }
+    DoubleInterval operator*(const DoubleInterval& that)
+    {
+        long double aa = static_cast<double>(this->from) * static_cast<double>(that.from);
+        long double ab = static_cast<double>(this->from) * static_cast<double>(that.to);
+        long double ba = static_cast<double>(this->to) * static_cast<double>(that.from);
+        long double bb = static_cast<double>(this->to) * static_cast<double>(that.to);
+        from = std::min(aa, std::min(ab, std::min(ba, bb)));
+        to = std::max(aa, std::max(ab, std::max(ba, bb)));
+        return *this;
+    }
+    DoubleInterval operator/(const DoubleInterval& that)
+    {
+        long double aa = static_cast<double>(this->from) / static_cast<double>(that.from);
+        long double ab = static_cast<double>(this->from) / static_cast<double>(that.to);
+        long double ba = static_cast<double>(this->to) / static_cast<double>(that.from);
+        long double bb = static_cast<double>(this->to) / static_cast<double>(that.to);
+        from = std::min(aa, std::min(ab, std::min(ba, bb)));
+        to = std::max(aa, std::max(ab, std::max(ba, bb)));
+        return *this;
+    }
+    operator double() 
+    {
+    return static_cast<double>(from);
     }
 };
 
