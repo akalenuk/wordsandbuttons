061fad6ab98200598d21f287713f7f9313f2e577
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-08-03 10:12:45 +0300

    namespaces

--- exp/sort/radix/main.cpp
+++ exp/sort/radix/main.cpp
@@ -9,17 +9,19 @@
 #include <fstream>
 #include <chrono>
 
+using namespace std::chrono;
+using namespace std;
 
 void functional_tests() {
-    std::vector<std::string> unsorted = {"cat", "pat", "bed", "test", "test but longer", "test"};
-    std::vector<std::string> std_sorted(unsorted.begin(), unsorted.end());
-    std::sort(std_sorted.begin(), std_sorted.end());
+    vector<string> unsorted = {"cat", "pat", "bed", "test", "test but longer", "test"};
+    vector<string> std_sorted(unsorted.begin(), unsorted.end());
+    sort(std_sorted.begin(), std_sorted.end());
     Trie::Set<4> trie_set;
     for(const auto& s : unsorted)
         trie_set.store(s.c_str());
 
     // sort test
-    std::vector<std::string> sorted;
+    vector<string> sorted;
     Trie::Set<4>::fill_vector_sorted(&trie_set, sorted);
     for(auto i =0u; i < unsorted.size(); ++i)
         assert(sorted[i] == std_sorted[i]);
@@ -31,7 +33,7 @@ void functional_tests() {
     assert(! trie_set.contains("not"));
 
     // primitive tests for map
-    Trie::Map<std::string, 4> trie_map;
+    Trie::Map<string, 4> trie_map;
     for(const auto& s : unsorted)
         trie_map.store(s.c_str(), s);
 
@@ -43,26 +45,26 @@ void functional_tests() {
 }
 
 template <unsigned int RADIX_BITS> 
-void radix_sort_performance_print(const std::vector<std::string>& words) {
-    auto radix_start = std::chrono::high_resolution_clock::now();
+void radix_sort_performance_print(const vector<string>& words) {
+    auto radix_start = high_resolution_clock::now();
     Trie::Set<RADIX_BITS> trie;
     for(const auto& word : words)
         trie.store(word.c_str());
-    std::vector<std::string> sorted_words;
+    vector<string> sorted_words;
     sorted_words.reserve(words.size());
     Trie::Set<RADIX_BITS>::fill_vector_sorted(&trie, sorted_words);
-    auto radix_sort_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - radix_start).count();
-    std::cout << "   radix " << RADIX_BITS << " sort - " << radix_sort_duration << "\n";
+    auto radix_sort_duration = duration_cast<microseconds>(high_resolution_clock::now() - radix_start).count();
+    cout << "   radix " << RADIX_BITS << " sort - " << radix_sort_duration << "\n";
  }
 
 
 void sort_performance_prints() {
     // load a dictionary
-    std::ifstream dict("en-US.dic");
-    std::vector<std::string> reversed_words;
-    std::string word;
-    while (std::getline(dict, word)) {
-        std::string reversed_word(word.rbegin(), word.rend());
+    ifstream dict("en-US.dic");
+    vector<string> reversed_words;
+    string word;
+    while (getline(dict, word)) {
+        string reversed_word(word.rbegin(), word.rend());
         reversed_words.push_back(reversed_word);
     }
     // inflate 64 times
@@ -74,11 +76,11 @@ void sort_performance_prints() {
     
 
     // std::sort
-    auto std_start = std::chrono::high_resolution_clock::now();
-    std::vector<std::string> std_sorted_reversed_words(reversed_words.begin(), reversed_words.end());
-    std::sort(std_sorted_reversed_words.begin(), std_sorted_reversed_words.end());
-    auto std_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - std_start).count();
-    std::cout << "   std::sort - " << std_duration << "\n";
+    auto std_start = high_resolution_clock::now();
+    vector<string> std_sorted_reversed_words(reversed_words.begin(), reversed_words.end());
+    sort(std_sorted_reversed_words.begin(), std_sorted_reversed_words.end());
+    auto std_duration = duration_cast<microseconds>(high_resolution_clock::now() - std_start).count();
+    cout << "   std::sort - " << std_duration << "\n";
 
     // radix sort
     radix_sort_performance_print<1>(reversed_words);
