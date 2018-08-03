4a4fedfa2f95b10a9eb6429aee44843f8762854b
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-08-03 11:55:07 +0300

    fake dictionary generator

--- exp/sort/radix/main.cpp
+++ exp/sort/radix/main.cpp
@@ -10,9 +10,25 @@
 #include <chrono>
 #include <map>
 #include <unordered_map>
+#include <random>
 
 using namespace std::chrono;
 using namespace std;
+
+vector<string> made_up_words(unsigned int how_much, unsigned int smallest, unsigned int largest) {
+    vector<string> words;
+    std::mt19937 rng(0);
+    std::uniform_int_distribution<unsigned int> word_sizes(smallest, largest);
+    std::uniform_int_distribution<char> word_letter(' ', 'z');
+    for(auto i = 0u; i < how_much; ++i) {
+        auto word_size = word_sizes(rng);
+        string word;
+        for(auto j = 0; j < word_size; ++j)
+            word.push_back(word_letter(rng));
+        words.push_back(word);
+    }
+    return words;
+}
 
 void functional_tests() {
     vector<string> unsorted = {"cat", "pat", "bed", "test", "test but longer", "test"};
