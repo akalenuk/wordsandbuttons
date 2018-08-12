aae3129d8144d15a254ee5210208c591db8266f1
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-08-12 08:06:42 +0300

    response with request

--- exp/webserver/webserver.c
+++ exp/webserver/webserver.c
@@ -9,7 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#define VERSION 23
+#define VERSION 1
 #define BUFSIZE 8096
 #define ERROR      42
 #define LOG        44
@@ -20,21 +20,6 @@
 #   define SIGCLD SIGCHLD
 #endif
 
-struct {
-  char *ext;
-  char *filetype;
-} extensions [] = {
-  {"gif", "image/gif" },
-  {"jpg", "image/jpg" },
-  {"jpeg","image/jpeg"},
-  {"png", "image/png" },
-  {"ico", "image/ico" },
-  {"zip", "image/zip" },
-  {"gz",  "image/gz"  },
-  {"tar", "image/tar" },
-  {"htm", "text/html" },
-  {"html","text/html" },
-  {0,0} };
 
 /* this is a child web server process, so we can exit on errors */
 void web(int fd)
@@ -42,60 +27,36 @@ void web(int fd)
   int j, file_fd, buflen;
   long i, ret, len;
   char * fstr;
-  static char buffer[BUFSIZE+1]; /* static so zero filled */
+  static char request[BUFSIZE+1]; 
+  static char response[BUFSIZE+1]; 
 
-  ret =read(fd,buffer,BUFSIZE);   /* read Web request in one go */
+  ret =read(fd,request,BUFSIZE);   /* read Web request in one go */
   if(ret == 0 || ret == -1) {  /* read failure stop now */
     return;
   }
   if(ret > 0 && ret < BUFSIZE)  /* return code is valid chars */
-    buffer[ret]=0;    /* terminate the buffer */
-  else buffer[0]=0;
-  for(i=0;i<ret;i++)  /* remove CF and LF characters */
-    if(buffer[i] == '\r' || buffer[i] == '\n')
-      buffer[i]='*';
-  if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) {
+    request[ret]=0;    /* terminate the buffer */
+  else 
+    request[0]=0;
+
+  if( strncmp(request,"GET ",4) && strncmp(request,"get ",4) ) {
     return;
   }
+  int request_size = 0;
   for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
-    if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
-      buffer[i] = 0;
+    if(request[i] == ' ') { /* string is "GET URL " +lots of other stuff */
+      request[i] = 0;
+      request_size = i;
       break;
     }
   }
-  for(j=0;j<i-1;j++)   /* check for illegal parent directory use .. */
-    if(buffer[j] == '.' && buffer[j+1] == '.') {
-      return;
-    }
-  if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
-    (void)strcpy(buffer,"GET /index.html");
+  if( !strncmp(&request[0],"GET /\0",6) || !strncmp(&request[0],"get /\0",6) ) /* convert no filename to index file */
+    (void)strcpy(request,"GET /index.html");
 
-  /* work out the file type and check we support it */
-  buflen=strlen(buffer);
-  fstr = (char *)0;
-  for(i=0;extensions[i].ext != 0;i++) {
-    len = strlen(extensions[i].ext);
-    if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
-      fstr =extensions[i].filetype;
-      break;
-    }
-  }
-  if(fstr == 0) 
-    return;
+  (void)sprintf(response,"HTTP/1.1 200 OK\nServer: webserver/%d.0\nContent-Length: %d\nConnection: close\nContent-Type: text/html\n\n", VERSION, request_size); /* Header + a blank line */
+  (void)strcat(response, request);
+  (void)write(fd, response, strlen(response));
 
-  if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
-    return;
-  }
-  len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */
-        (void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
-  
-  (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
-  (void)write(fd,buffer,strlen(buffer));
-
-  /* send file in 8KB block - last block may be smaller */
-  while (  (ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
-    (void)write(fd,buffer,ret);
-  }
   sleep(1);  /* allow socket to drain before signalling the socket is closed */
   close(fd);
   exit(1);
@@ -109,19 +70,7 @@ int main(int argc, char **argv)
   static struct sockaddr_in serv_addr; /* static = initialised to zeros */
 
   if( argc < 3  || argc > 3 || !strcmp(argv[1], "-?") ) {
-    (void)printf("hint: nweb Port-Number Top-Directory\t\tversion %d\n\n"
-  "\tnweb is a small and very safe mini web server\n"
-  "\tnweb only servers out file/web pages with extensions named below\n"
-  "\t and only from the named directory or its sub-directories.\n"
-  "\tThere is no fancy features = safe and secure.\n\n"
-  "\tExample: nweb 8181 /home/nwebdir &\n\n"
-  "\tOnly Supports:", VERSION);
-    for(i=0;extensions[i].ext != 0;i++)
-      (void)printf(" %s",extensions[i].ext);
-
-    (void)printf("\n\tNot Supported: URLs including \"..\", Java, Javascript, CGI\n"
-  "\tNot Supported: directories / /etc /bin /lib /tmp /usr /dev /sbin \n"
-  "\tNo warranty given or implied\n\tNigel Griffiths nag@uk.ibm.com\n"  );
+    (void)printf("hint: webserver Port-Number Top-Directory\t\tversion %d\n\n", VERSION);
     exit(0);
   }
   if( !strncmp(argv[2],"/"   ,2 ) || !strncmp(argv[2],"/etc", 5 ) ||
