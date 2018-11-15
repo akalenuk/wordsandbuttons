550d6799a2cb46e38782cb759728bf14899aa3a5
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-11-15 20:48:11 +0200

    tool for drafts rstripping

---
+++ tools/rstrip_html/drafts_rstrip_html.py
@@ -0,0 +1,14 @@
+import os
+
+path = '../../drafts/'
+for file_name in os.listdir(path):
+    if os.path.isfile(path + file_name):
+        if file_name.endswith('.html'):
+            print file_name
+            html = open(path + file_name, 'r')
+            lines = html.readlines()
+            html.close()
+            rstripped = [line.rstrip() + '\n' for line in lines]
+            html = open(path + file_name, 'w')
+            html.writelines(rstripped)
+            html.close()
