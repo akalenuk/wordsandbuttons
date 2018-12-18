b46bc763233b598ca4380b0bba16ed18a4e7d082
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-12-18 20:39:15 +0200

    initial benchmark

---
+++ exp/cpp_and_and/benchmark.cpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <array>
+
+int main() {
+    using TheType = volatile int;
+    constexpr auto TheSize = 65536 * 16;
+    std::mt19937 rng(0);
+    std::uniform_int_distribution<TheType> distribution(0, 1);
+    std::array<TheType, TheSize> xs;
+    for (TheType &digit : xs) {
+        digit = distribution(rng);
+    }
+   
+    volatile auto four_1_in_a_row = 0u;
+    auto start = std::chrono::system_clock::now();
+    for (auto i = 0u; i < TheSize - 3; ++i)
+        if(xs[i] == 1 && xs[i+1] == 1 && xs[i+2] == 1 && xs[i+3] == 1)
+            four_1_in_a_row++; 
+    auto end = std::chrono::system_clock::now();
+
+    std::cout << (end-start).count() << "\n";
+}
