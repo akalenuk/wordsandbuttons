bfe9ec07b24e71fb9b31cfe352129f12b1d4ede2
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-12-30 15:23:54 +0200

    ARMv7 experiments 10x less iterations

---
+++ exp/cpp_and_and/ARMv7/minus_mul_plus.cpp
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <array>
+
+inline int sq(int x) {
+    return x*x;
+}
+
+int main() {
+    using TheType = int;
+    constexpr auto TheSize = 16 * 1000000;
+    std::mt19937 rng(0);
+    std::uniform_int_distribution<TheType> distribution(0, 1);
+    std::vector<TheType> xs(TheSize);
+    for (TheType &digit : xs) {
+        digit = distribution(rng);
+    }
+   
+    volatile auto four_1_in_a_row = 0u;
+    auto start = std::chrono::system_clock::now();
+    for (auto i = 0u; i < TheSize - 3; i += 1)
+        if(sq(xs[i] - 1) + sq(xs[i+1] - 1) + sq(xs[i+2] - 1) + sq(xs[i+3] - 1) == 0)
+            four_1_in_a_row++; 
+    auto end = std::chrono::system_clock::now();
+    
+    std::cout << "time: " << (end-start).count() * 1e-9  << "  1111s: " << four_1_in_a_row << "\n";
+}
