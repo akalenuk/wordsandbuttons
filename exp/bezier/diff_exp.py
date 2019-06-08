50d7703abf5256a73ea18ffb923786a84b77a4dc
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-06-08 20:18:31 +0300

    error

--- exp/bezier/diff_exp.py
+++ exp/bezier/diff_exp.py
@@ -12,6 +12,6 @@ x1, y1, x2, y2, yx1, yx2 = symbols('x1 y1 x2 y2 yx1 yx2')
 
 # constant speed
 eq = Derivative(Derivative(x(t), t)**2 + Derivative(y(t), t)**2, t)
-res = dsolve(eq, (x(t), y(t)), ics={x(0):x1, y(0):y1, x(1):x2, y(1):y2, y(0)/x(0):yx1, y(1)/x(1):yx2})
+res = dsolve(eq, ics={x(0):x1, y(0):y1, x(1):x2, y(1):y2, y(0)/x(0):yx1, y(1)/x(1):yx2})
 
 print (res)
