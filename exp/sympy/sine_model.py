112e60c6eadc21b9c8765c1de432a28b69de7b07
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-06-13 21:50:11 +0300

    somtheing wrong in my logic

---
+++ exp/sympy/sine_model.py
@@ -0,0 +1,28 @@
+from sympy import *
+from math import pi
+
+a, b, c, d, e, f, g, x = symbols('a b c d e f g x')
+
+sine = a*x**6 + b*x**5 + c*x**4 + d*x**3 + e*x**2 + f*x + g
+sine_d = diff(sine, x)
+sine_i = integrate(sine, x)
+
+the_system = [sine_i.subs(x, pi) - sine_i.subs(x, 0) - 1,  
+    sine_d.subs(x, 0) - 1, 
+    sine_d.subs(x, pi / 2), 
+    sine_d.subs(x, pi) + 1, 
+    sine.subs(x, 0), 
+    sine.subs(x, pi / 2) - 1, 
+    sine.subs(x, pi), 
+]
+
+for expr in the_system:
+    print(expr)
+
+res = solve(the_system,
+ (a, b, c, d, e, f, g))
+
+for exp in res:
+#    print(srepr(exp))
+    print(exp)
+    print('')
