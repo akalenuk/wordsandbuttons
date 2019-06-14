5a56b4028fa5e2eeea6ae7ae299ec2b2b17823ae
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-06-14 20:01:27 +0300

    still nothing

--- exp/sympy/sine_model.py
+++ exp/sympy/sine_model.py
@@ -1,5 +1,4 @@
 from sympy import *
-from math import pi
 
 a, b, c, d, e, f, g, x = symbols('a b c d e f g x')
 
@@ -7,13 +6,14 @@ sine = a*x**6 + b*x**5 + c*x**4 + d*x**3 + e*x**2 + f*x + g
 sine_d = diff(sine, x)
 sine_i = integrate(sine, x)
 
-the_system = [sine_i.subs(x, pi) - sine_i.subs(x, 0) - 1,  
+the_system = [
+    sine_i.subs(x, pi) - sine_i.subs(x, 0) - 1,  
     sine_d.subs(x, 0) - 1, 
     sine_d.subs(x, pi / 2), 
     sine_d.subs(x, pi) + 1, 
     sine.subs(x, 0), 
     sine.subs(x, pi / 2) - 1, 
-    sine.subs(x, pi), 
+    sine.subs(x, pi) 
 ]
 
 for expr in the_system:
