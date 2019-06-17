527d1d587bb502b6af7078bed2f8c3c5c0fb736c
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-06-17 08:51:57 +0300

    working arcs

--- exp/sympy/arcs.py
+++ exp/sympy/arcs.py
@@ -7,14 +7,18 @@ arc_y = y0 + cos(t) * r
 x1, y1, dyx1, x2, y2, dyx2, t1, t2, tl = symbols('x1 y1 dyx1 x2 y2 dyx2 t1 t2 tl')
 
 
-res = solve(
+t1t2 = solve(
+   [(diff(arc_y, t) / diff(arc_x, t)).subs(t, t1) - dyx1,
+    (diff(arc_y, t) / diff(arc_x, t)).subs(t, t2) - dyx2
+], (t1, t2))
+
+print(t1t2[-1]) 
+
+tlx0y0r = solve(
    [arc_x.subs(t, t1) - x1, 
     arc_y.subs(t, t1) - y1, 
-    (diff(arc_y, t) / diff(arc_x, t)).subs(t, t1) - dyx1,
     arc_x.subs(t, t2) - (x2 + tl) , 
     arc_y.subs(t, t2) - (y2 + tl*dyx2), 
-    (diff(arc_y, t) / diff(arc_x, t)).subs(t, t2) - dyx2
-], (t1, t2, tl, x0, y0, r))
+], (tl, x0, y0, r))
 
-for exp in  res:
-    print(srepr(exp))
+print(tlx0y0r)
