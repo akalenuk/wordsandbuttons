880bda4306f245933ae49a1c25b44759f580eda2
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-06-22 20:25:51 +0300

    sine-like cubic

--- exp/sympy/not_sine_model.py
+++ exp/sympy/not_sine_model.py
@@ -1,22 +1,19 @@
 from sympy import *
 from math import pi
 
-a, b, c, d, e, f, x, px, py = symbols('a b c d e f x px py')
+a, b, c, d, x, px, py = symbols('a b c d x px py')
 
-sine = a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
+sine = a*x**3 + b*x**2 + c*x + d
 sine_d = diff(sine, x)
-sine_i = integrate(sine, x)
 
 the_system = [
-    sine_d.subs(x, 0) - 1, 
-    sine_d.subs(x, pi / 2), 
-    sine_d.subs(x, pi) + 1, 
+    sine_d.subs(x, px), 
     sine.subs(x, 0), 
     sine.subs(x, px) - py, 
     sine.subs(x, pi) 
 ]
 
-res = solve(the_system, (a, b, c, d, e, f))
+res = solve(the_system, (a, b, c, d))
 
 for var, exp in res.items():
     print(var, srepr(exp))
