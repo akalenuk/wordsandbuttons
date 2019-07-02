eff762849a8e78edba606d774840406d464105b4
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-07-02 20:19:53 +0300

    2 arcs draft

---
+++ exp/sympy/arcs2.py
@@ -0,0 +1,26 @@
+from sympy import *
+
+xc1, yc1, r1, xc2, yc2, r2, t = symbols('xc1 yc1 r1 xc2 yc2 r2 t')
+
+arc1_x = xc1 + sin(t) * r
+arc1_y = yc1 + cos(t) * r
+arc2_x = xc2 + sin(t) * r
+arc2_y = yc2 + cos(t) * r
+
+x1, y1, dyx1, x2, y2, dyx2, t1, t2 = symbols('x1 y1 dyx1 x2 y2 dyx2 t1 t2')
+
+
+
+solution = solve(
+   [
+    t1 - t2 - pi,
+    x1 + r - xc1,
+    y1 + r * dyx1 - yc1,
+    x2 + r - xc2,
+    y2 + r * dyx2 - yc2,
+    arc1_x.subs(t, t1) - arc2_x(t, t2), 
+    arc1_y.subs(t, t1) - arc2_y(t, t2) 
+], (t1, xc1, yc1, t2, xc2, yc2, t1, r))
+
+for var, exp in solution.items():
+    print('var ' + jscode(var) + ' = ' + jscode(exp))
