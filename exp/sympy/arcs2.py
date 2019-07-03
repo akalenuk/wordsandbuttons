b7fb79f9158d1ac1418c353c8f3de42f5b09a4dc
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-07-03 22:37:47 +0300

    new system takes hours with no result

--- exp/sympy/arcs2.py
+++ exp/sympy/arcs2.py
@@ -7,20 +7,22 @@ arc1_y = yc1 + cos(t) * r
 arc2_x = xc2 + sin(t) * r
 arc2_y = yc2 + cos(t) * r
 
-x1, y1, dyx1, x2, y2, dyx2, t1, t2 = symbols('x1 y1 dyx1 x2 y2 dyx2 t1 t2')
+x1, y1, dyx1, x2, y2, dyx2, t1, t2, r1, r2 = symbols('x1 y1 dyx1 x2 y2 dyx2 t1 t2 r1 r2')
 
 
 
 solutions = solve(
    [
     t1 - t2 - pi,
-    x1 + r - xc1,
-    y1 + r * dyx1 - yc1,
-    x2 + r - xc2,
-    y2 + r * dyx2 - yc2,
+    x1 + r1 - xc1,
+    y1 + r1 * dyx1 - yc1,
+    x2 + r2 - xc2,
+    y2 + r2 * dyx2 - yc2,
+    (r1 - xc1)**2 + (r1*dyx1 - xc1)**2 - r,
+    (r2 - xc2)**2 + (r2*dyx2 - xc2)**2 - r,
     arc1_x.subs(t, t1) - arc2_x.subs(t, t2), 
-    arc1_y.subs(t, t1) - arc2_y.subs(t, t2) 
-], (t1, xc1, yc1, t2, xc2, yc2, r))
+    arc1_y.subs(t, t1) - arc2_y.subs(t, t2), 
+], (t1, xc1, yc1, t2, xc2, yc2, r1, r2, r))
 
 for solution in solutions:
     print('var t1 = ' + jscode(solution[0]))
@@ -29,4 +31,6 @@ for solution in solutions:
     print('var t2 = ' + jscode(solution[3]))
     print('var xc2 = ' + jscode(solution[4]))
     print('var yc2 = ' + jscode(solution[5]))
-    print('var r = ' + jscode(solution[6]))
+    print('var r1 = ' + jscode(solution[6]))
+    print('var r2 = ' + jscode(solution[7]))
+    print('var r = ' + jscode(solution[8]))
