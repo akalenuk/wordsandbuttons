1d90e7b12bdc5e5533912691c90df472952a5780
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-07-28 09:58:44 +0300

    cosmetics

--- exp/non_polynomial_splines/arc_and_line.py
+++ exp/non_polynomial_splines/arc_and_line.py
@@ -4,18 +4,20 @@ from sympy import *
 x1, y1, dx1, dy1 = symbols('x1 y1 dx1 dy1')
 x2, y2, dx2, dy2 = symbols('x2 y2 dx2 dy2')
 
-ix, iy = symbols('ix, iy')
+# intermediate
+ix, iy = symbols('ix iy')
 
+# output
 ax1, ay1, r1, t2 = symbols('ax1 ay1 r1 t2')
 
 solutions = solve(
    [
-    x1 - r1*dy1 - ax1,
-    y1 + r1*dx1 - ay1,
-    x2 + dx2 * t2 - ix,
-    y2 + dy2 * t2 - iy,
-    (ix-ax1)**2 + (iy-ay1)**2 - r1**2,
-    (ix-ax1)*(ix-x2) + (iy-ay1)*(iy-y2)
+    x1 - r1*dy1 - ax1, # arc radius is orthogonal to (x1, y1)
+    y1 + r1*dx1 - ay1, #
+    x2 + dx2 * t2 - ix,  # intersection point is on the tangent line of (x2, y2)
+    y2 + dy2 * t2 - iy,  #
+    (ix-ax1)*(ix-ax1) + (iy-ay1)*(iy-ay1) - r1*r1,  # intersection point is on the arc
+    (ix-ax1)*(ix-x2)  + (iy-ay1)*(iy-y2)    # intersection is only touching the arc
 ], (ax1, ay1, r1, t2, ix, iy))
 
 for solution in solutions:
