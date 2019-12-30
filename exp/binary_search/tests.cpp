23d0298622f5eff0723b23b0b6adbc10e0725219
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-12-30 17:14:05 +0200

    chrono

--- exp/binary_search/tests.cpp
+++ exp/binary_search/tests.cpp
@@ -28,7 +28,16 @@ vector<string> made_up_words(unsigned int how_much, unsigned int smallest, unsig
     return words;
 }
 
- 
+#define MEASURE(CODE_TO_MEASURE, NAME_TO_PRINT) \
+    { \
+    ResetData();    \
+    auto start = std::chrono::system_clock::now(); \
+    CODE_TO_MEASURE \
+    auto end = std::chrono::system_clock::now(); \
+    std::chrono::duration<double> difference = end - start; \
+    std::cout << difference.count() << " - " << NAME_TO_PRINT << "\n";    \
+    }
+
 
 int main() {
     return 0;
