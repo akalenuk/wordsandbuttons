579dead4db677d974eacaa9d9775306bcfebc085
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-01-02 19:39:32 +0200

    benchmark 1

--- exp/binary_search/tests.cpp
+++ exp/binary_search/tests.cpp
@@ -1,4 +1,3 @@
-
 #include <array>
 #include <algorithm>
 #include <iostream>
@@ -13,6 +12,8 @@
 using namespace std::chrono;
 using namespace std;
 
+static const size_t NONE = std::numeric_limits<size_t>::max();
+
 vector<int> uniform_random(size_t how_much, int from, int to) {
     vector<int> ints(how_much);
     std::mt19937 rng(0);
@@ -24,7 +25,6 @@ vector<int> uniform_random(size_t how_much, int from, int to) {
 
 #define MEASURE(CODE_TO_MEASURE, NAME_TO_PRINT) \
     { \
-    ResetData();    \
     auto start = std::chrono::system_clock::now(); \
     CODE_TO_MEASURE \
     auto end = std::chrono::system_clock::now(); \
@@ -33,7 +33,31 @@ vector<int> uniform_random(size_t how_much, int from, int to) {
     }
 
 
+size_t find_if_any(const int what, const std::vector<int>& where, size_t from, size_t to) {
+    if(where[from] == what)
+        return from;
+    if(to - from <= 1)      
+      return NONE;
+    size_t mid = from + (to - from) / 2;
+    if(where[mid] < what)
+        return find_if_any(what, where, mid + 1, to);
+    return find_if_any(what, where, from, mid);
+}
+
+size_t find_if_any(const int what, const std::vector<int>& where) {
+    return find_if_any(what, where, 0, where.size() - 1);
+}
+
 int main() {
+    const auto test_data = uniform_random(100'000'000, 0, 100'000'000);
+    MEASURE(
+    for(auto i = 0; i < 100'000'000; ++i)
+        {
+        auto index = find_if_any(i, test_data);
+        if(index != NONE && test_data[index] != i)
+            std::cout << "search is faulty\n";
+        }
+    , "binary");
     return 0;
 }
 
