7dafe16ebba7bbed25b91912ebb6496b36822cb8
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-01-04 12:47:53 +0200

    forgot to sort

--- exp/binary_search/exp1.cpp
+++ exp/binary_search/exp1.cpp
@@ -20,6 +20,7 @@ vector<int> uniform_random(size_t how_much, int from, int to) {
     std::uniform_int_distribution<int> randoms(from, to);
     for(auto& i : ints)
         i = randoms(rng);
+    std::sort(ints.begin(), ints.end());
     return ints;
 }
 
