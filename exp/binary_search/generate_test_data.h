2ff51e425f1464e9179d4819db43fbcbe7c16c64
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-01-23 20:04:38 +0200

    two more distributions

--- exp/binary_search/generate_test_data.h
+++ exp/binary_search/generate_test_data.h
@@ -2,6 +2,7 @@
 #include <random>
 #include <vector>
 
+const double PI = 3.141592653589793;
 
 std::vector<int> uniform_random(size_t how_much, int from, int to) {
     std::vector<int> ints(how_much);
@@ -9,6 +10,30 @@ std::vector<int> uniform_random(size_t how_much, int from, int to) {
     std::uniform_int_distribution<int> randoms(from, to);
     for(auto& i : ints)
         i = randoms(rng);
+    std::sort(ints.begin(), ints.end());
+    return ints;
+}
+
+std::vector<int> skewed_to_high_end_random(size_t how_much, int from, int to) {
+    std::vector<int> ints(how_much);
+    std::random_device rng;
+    std::uniform_int_distribution<double> randoms(0., 1.);
+    for(auto& i : ints) {
+        double d = sin(randoms(rng) * PI / 2.);
+        i = static_cast<int>(from + (to - from) * d);
+        }
+    std::sort(ints.begin(), ints.end());
+    return ints;
+}
+
+std::vector<int> less_in_the_middle_random(size_t how_much, int from, int to) {
+    std::vector<int> ints(how_much);
+    std::random_device rng;
+    std::uniform_int_distribution<double> randoms(0., 1.);
+    for(auto& i : ints) {
+        double d = (sin(randoms(rng) * PI - PI / 2.) + 1.) / 2.;
+        i = static_cast<int>(from + (to - from) * d);
+        }
     std::sort(ints.begin(), ints.end());
     return ints;
 }
