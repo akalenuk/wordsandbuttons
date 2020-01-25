115ed446f86acc9c8aada1c5694f767b0a6d2776
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-01-25 14:51:27 +0200

    translation from js complete

--- exp/binary_search/find.h
+++ exp/binary_search/find.h
@@ -60,72 +60,13 @@ size_t find_by_interpolation(const int what, const std::vector<int>& where) {
 }
 
 
-/*
-function solved_linear(A, B, n){
-    var X = [];
-    for (var i = 0; i < n; ++i)
-        X.push(0.0);
-
-    // triangulize
-    for (var i = 0; i < n-1; ++i)
-        for (var j = 0; j < i+1; ++j) {
-            r = A[i+1][j] / underflow_padded(A[j][j]);
-            A[i+1][j] = 0.;
-            for (var b_j = j+1; b_j < n; ++b_j){
-                A[i+1][b_j] -= A[j][b_j]*r;
-            }
-            B[i+1] -= B[j]*r;
-        }
-
-    // calculate xs
-    X[n-1] = B[n-1] / underflow_padded(A[n-1][n-1]);
-    for (var i = n-2; i >= 0; --i){
-        var s = 0.0;
-        for (var j = i; j < n; ++j){
-            s = s + A[i][j]*X[j];
-        }
-        X[i] = (B[i] - s) / underflow_padded(A[i][i]);
-    }
-    return X;
-}
-
-function approximation_of(points, n) {
-    const N = points.length;
-    var A = [];
-    var B = [];
-
-    for (var i = 0; i < n; ++i){
-        var Ai = []
-        for (var j = 0; j < n; ++j){
-            Ai.push(0);
-            for(var k = 0; k < N; ++k){
-                Ai[j] += Math.pow(points[k][0], i + j);
-            }
-        }
-        A.push(Ai);
-        B.push(0);
-        for(var k = 0; k < N; ++k){
-            B[i] += points[k][1] * Math.pow(points[k][0], i);
-        }
-    }
-    var P = solved_linear(A, B,n);
-    return function(x) {
-        var Px = 0;
-        for(var i = 0; i < n; ++i) {
-            Px += P[i] * Math.pow(x, i);
-        }
-        return Px;
-    };
-}
-*/
-
-std::vector solve(std::vector<std::vector<double>>& A, std::vector<double>& B) {
+std::vector<double> solve(std::vector<std::vector<double>>& A, std::vector<double>& B) {
     const auto n = B.size();
 
     // triangulize
     for (auto i = 0u; i < n-1; ++i)
         for (auto j = 0u; j < i+1; ++j) {
-            const auto r = A[i+1][j] / underflow_padded(A[j][j]);
+            const auto r = A[i+1][j] / A[j][j];
             A[i+1][j] = 0.;
             for (auto b_j = j+1; b_j < n; ++b_j){
                 A[i+1][b_j] -= A[j][b_j]*r;
@@ -137,7 +78,7 @@ std::vector solve(std::vector<std::vector<double>>& A, std::vector<double>& B) {
     std::vector<double> X(n, 0.);
     X[n-1] = B[n-1] / A[n-1][n-1];
     for (auto i = n-2; i >= 0; --i){
-        var s = 0.0;
+        auto s = 0.0;
         for (auto j = i; j < n; ++j){
             s = s + A[i][j]*X[j];
         }
@@ -149,27 +90,27 @@ std::vector solve(std::vector<std::vector<double>>& A, std::vector<double>& B) {
 std::vector<double> polynomial_for(std::vector<int>& where, unsigned int n) {
     std::vector<std::vector<double>> A(n);
     for (auto& Ai : A)
-        Ai.resize(n, 0.)
+        Ai.resize(n, 0.);
     std::vector<double> B(n, 0.);
 
     for (auto i = 0u; i < n; ++i){
         auto& Ai = A[i];
         for (auto j = 0u; j < n; ++j){
-            for(auto k = 0u; k < N; ++k){
-                Ai[j] += std::pow(points[k][0], i + j);
+            for(auto k = 0u; k < where.size(); ++k){
+                Ai[j] += std::pow(where[k], i + j);
             }
         }
 
-        for(auto k = 0u; k < N; ++k){
-            B[i] += points[k][1] * std::pow(points[k][0], i);
+        for(auto k = 0u; k < where.size(); ++k){
+            B[i] += k * std::pow(where[k], i);
         }
     }
-    return solve(A, B, n);
+    return solve(A, B);
 }
 
 double Px(std::vector<double> P, double x) {
     auto y = 0;
-    for(auto i = 0u; i < n; ++i) {
+    for(auto i = 0u; i < P.size(); ++i) {
         y += P[i] * std::pow(x, i);
     }
     return y;
