e35463b5a4f21ea59221fde821a87b16a589787f
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-06-04 20:45:49 +0300

    python3

--- tools/rstrip_html/rstrip_html.py
+++ tools/rstrip_html/rstrip_html.py
@@ -4,7 +4,7 @@ path = '../../pages/'
 for file_name in os.listdir(path):
     if os.path.isfile(path + file_name):
         if file_name.endswith('.html'):
-            print file_name
+            print (file_name)
             html = open(path + file_name, 'r')
             lines = html.readlines()
             html.close()
