a84ae94e3f8f114ea98dea0af6574eda14c2e8d5
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-09-07 20:40:15 +0300

    ids for empty spans

---
+++ tools/make_ids_for_span/make_ids_for_span.py
@@ -0,0 +1,28 @@
+import os
+
+path = '../../pages/'
+
+def text_to_id(text):
+	allowed = 'qwertyuiopasdfghjklzxcvbnm_'
+	return ''.join([c if c in allowed else '_' for c in text.lower()])
+
+
+for file_name in os.listdir(path):
+	if os.path.isfile(path + file_name):
+		if file_name.endswith('.html'):
+			print (file_name)
+			html = open(path + file_name, 'r')
+			text = html.read()
+			html.close()
+			spans = [s.split('</span>')[0] for s in text.split('<span>')[1:]]
+			Spans = [S.split('</Span>')[0] for S in text.split('<Span>')[1:]]
+			if len(spans) + len(Spans) == 0:
+				continue
+			new_text = text
+			for span in spans:
+				new_text = new_text.replace('<span>' + span, '<span id="' + text_to_id(span) + '">' + span)
+			for Span in Spans:
+				new_text = new_text.replace('<Span>' + Span, '<Span id="' + text_to_id(Span) + '">' + Span)
+			html = open(path + file_name, 'w')
+			html.write(new_text)
+			html.close()
