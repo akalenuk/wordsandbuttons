52158555167c08ca0c1d78386e4b7789dd683819
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-09-26 16:41:31 +0300

    parametric simplify

--- exp/rational_bounds/src/lib.rs
+++ exp/rational_bounds/src/lib.rs
@@ -1,31 +1,42 @@
 // Subset of rational numbers in u32/u32
-struct R32 {
-    n : u32,  // numerator
-    d : u32,  // denominator
+struct R<U> {
+    n : U,  // numerator
+    d : U,  // denominator
     f : bool, // finite
     p : bool  // positive
 }
 
 // Rational bounds structure
 struct RB32 {
-    lb : R32, // lower bound
-    ub : R32  // upper bound
+    lb : R<u32>, // lower bound
+    ub : R<u32>  // upper bound
 }
 
-fn simplify(x: R32) -> R32 {
-    if !x.f || x.n == 0 { 
+fn simplify<U>(x: R<U>) -> R<U> 
+    where U:std::cmp::PartialEq
+           + std::ops::Sub<Output = U>
+           + std::cmp::PartialOrd
+           + std::ops::Div<Output = U>
+           + std::convert::From<u32>
+           + std::marker::Copy
+{
+    if !x.f || x.n == U::from(0_u32) { 
         x
     } else {
-        fn gcd(a:u32, b:u32) -> u32 {
+        fn gcd<U>(a:U, b:U) -> U 
+            where U: std::ops::Sub<Output = U>
+                   + std::cmp::PartialOrd
+                   + Copy
+        {
             if a > b {gcd(a - b, b)}
             else if a < b {gcd(a, b - a)}
             else {a}
         }
         let cd = gcd(x.n, x.d); // common divisor
-        if cd == 1 { 
+        if cd == U::from(1_u32) {
             x
         } else {
-            simplify(R32{n: x.n / cd, d: x.d / cd, f: true, p: x.p})
+            simplify(R::<U>{n: x.n / cd, d: x.d / cd, f: true, p: x.p})
         }
     }
 }
@@ -36,7 +47,7 @@ mod tests {
 
     #[test]
     fn simplification_stops() {
-        let simplifiable = R32{n:0, d:1, f:true, p:true};
+        let simplifiable = R::<u32>{n:0, d:1, f:true, p:true};
         let simplified = simplify(simplifiable);
         assert_eq!(simplified.n, 0);
         assert_eq!(simplified.d, 1);
@@ -44,7 +55,15 @@ mod tests {
 
     #[test]
     fn simplification_works() {
-        let simplifiable = R32{n:3*5*7*11, d:2*5*7*13, f:true, p:true};
+        let simplifiable = R::<u32>{n:3*5*7*11, d:2*5*7*13, f:true, p:true};
+        let simplified = simplify(simplifiable);
+        assert_eq!(simplified.n, 3*11);
+        assert_eq!(simplified.d, 2*13);
+    }
+
+    #[test]
+    fn simplification_works_on_64_bits() {
+        let simplifiable = R::<u64>{n:3*5*7*11, d:2*5*7*13, f:true, p:true};
         let simplified = simplify(simplifiable);
         assert_eq!(simplified.n, 3*11);
         assert_eq!(simplified.d, 2*13);
