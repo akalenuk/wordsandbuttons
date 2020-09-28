951884d559acb5476b64a5a105856636e19a4812
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-09-28 21:02:44 +0300

    partial ordering

--- exp/rational_bounds/src/lib.rs
+++ exp/rational_bounds/src/lib.rs
@@ -4,73 +4,127 @@ impl U32U64Only for u64 {}
 
 // Subset of rational numbers in u32/u32 or u64/u64
 struct R<U:U32U64Only> {
-    n : U,  // numerator
-    d : U,  // denominator
-    f : bool, // finite
-    p : bool  // positive
+	n : U,  // numerator
+	d : U,  // denominator
+	p : bool  // positive
 }
 
 // Rational bounds structure
 struct RB32 {
-    lb : R<u32>, // lower bound
-    ub : R<u32>  // upper bound
+	lb : R<u32>, // lower bound
+	ub : R<u32>  // upper bound
 }
 
 fn simplify<U>(x: R<U>) -> R<U> 
-    where U:std::cmp::PartialEq
-           + std::ops::Sub<Output = U>
-           + std::cmp::PartialOrd
-           + std::ops::Div<Output = U>
-           + std::convert::From<u32>
-           + std::marker::Copy
-           + U32U64Only
+	where U:std::cmp::PartialEq
+			+ std::ops::Sub<Output = U>
+			+ std::cmp::PartialOrd
+			+ std::ops::Div<Output = U>
+			+ std::convert::From<u32>
+			+ std::marker::Copy
+			+ U32U64Only
 {
-    if !x.f || x.n == U::from(0_u32) { 
-        x
-    } else {
-        fn gcd<U>(a:U, b:U) -> U 
-            where U: std::ops::Sub<Output = U>
-                   + std::cmp::PartialOrd
-                   + Copy
-        {
-            if a > b {gcd(a - b, b)}
-            else if a < b {gcd(a, b - a)}
-            else {a}
-        }
-        let cd = gcd(x.n, x.d); // common divisor
-        if cd == U::from(1_u32) {
-            x
-        } else {
-            simplify(R::<U>{n: x.n / cd, d: x.d / cd, f: true, p: x.p})
-        }
-    }
+	if x.n == U::from(0_u32) || x.d == U::from(0_u32) { 
+		x
+	} else {
+		fn gcd<U>(a:U, b:U) -> U 
+			where U: std::ops::Sub<Output = U>
+					+ std::cmp::PartialOrd
+					+ Copy
+		{
+			if a > b {gcd(a - b, b)}
+			else if a < b {gcd(a, b - a)}
+			else {a}
+		}
+		let cd = gcd(x.n, x.d); // common divisor
+		if cd == U::from(1_u32) {
+			x
+		} else {
+			simplify(R::<U>{n: x.n / cd, d: x.d / cd, p: x.p})
+		}
+	}
+}
+
+impl std::cmp::PartialOrd for R<u32> {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		if self.p && !other.p {
+			Some(std::cmp::Ordering::Greater)
+		} else if !self.p && other.p {
+			Some(std::cmp::Ordering::Less)
+		} else {
+			// self.p = other.p
+			let a = if self.p {self.n as u64 * other.d as u64} else {other.n as u64 * self.d as u64};
+			let b = if other.p {other.n as u64 * self.d as u64} else {self.n as u64 * other.d as u64};
+			Some(a.cmp(&b))
+		}
+	}
+}
+
+impl std::cmp::PartialEq for R<u32> {
+	fn eq(&self, other: &Self) -> bool {
+		self.p == other.p && self.n == other.n && self.d == other.d
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn simplification_stops() {
-        let simplifiable = R::<u32>{n:0, d:1, f:true, p:true};
-        let simplified = simplify(simplifiable);
-        assert_eq!(simplified.n, 0);
-        assert_eq!(simplified.d, 1);
-    }
+	#[test]
+	fn simplification_stops() {
+		let simplifiable = R::<u32>{n:0, d:1, p:true};
+		let simplified = simplify(simplifiable);
+		assert_eq!(simplified.n, 0);
+		assert_eq!(simplified.d, 1);
+	}
 
-    #[test]
-    fn simplification_works() {
-        let simplifiable = R::<u32>{n:3*5*7*11, d:2*5*7*13, f:true, p:true};
-        let simplified = simplify(simplifiable);
-        assert_eq!(simplified.n, 3*11);
-        assert_eq!(simplified.d, 2*13);
-    }
+	#[test]
+	fn simplification_works() {
+		let simplifiable = R::<u32>{n:3*5*7*11, d:2*5*7*13, p:true};
+		let simplified = simplify(simplifiable);
+		assert_eq!(simplified.n, 3*11);
+		assert_eq!(simplified.d, 2*13);
+	}
 
-    #[test]
-    fn simplification_works_on_64_bits() {
-        let simplifiable = R::<u64>{n:3*5*7*11, d:2*5*7*13, f:true, p:true};
-        let simplified = simplify(simplifiable);
-        assert_eq!(simplified.n, 3*11);
-        assert_eq!(simplified.d, 2*13);
-    }
+	#[test]
+	fn simplification_works_on_64_bits() {
+		let simplifiable = R::<u64>{n:3*5*7*11, d:2*5*7*13, p:true};
+		let simplified = simplify(simplifiable);
+		assert_eq!(simplified.n, 3*11);
+		assert_eq!(simplified.d, 2*13);
+	}
+
+	#[test]
+	fn comparison_on_r32() {
+		let a = R::<u32>{n:1, d:2, p:true};
+		let b = R::<u32>{n:3, d:5, p:true};
+		assert!(a < b);
+		assert!(b > a);
+		assert!(a != b);
+	}
+
+	#[test]
+	fn comparison_on_negative_r32() {
+		let a = R::<u32>{n:1, d:2, p:true};
+		let b = R::<u32>{n:3, d:5, p:false};
+		assert!(a > b);
+		assert!(b < a);
+		assert!(b != a);
+	}
+
+	#[test]
+	fn comparison_on_other_negative_r32() {
+		let b = R::<u32>{n:1, d:2, p:true};
+		let a = R::<u32>{n:3, d:5, p:false};
+		assert!(a < b);
+		assert!(b > a);
+		assert!(b != a);
+	}
+
+	#[test]
+	fn equality_on_r32() {
+		let a = R::<u32>{n:1, d:2, p:true};
+		let b = R::<u32>{n:1, d:2, p:true};
+		assert!(a == b);
+	}
 }
