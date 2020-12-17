1b9ac829a2d06f2076d29cbfea186ecabddb105e
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-12-17 15:47:15 +0200

    fixed bug with all_pages.html

--- tools/generate_things/generate_navigation.py
+++ tools/generate_things/generate_navigation.py
@@ -46,14 +46,16 @@ for filename in os.listdir(PAGES_DIR):
 	if filename == 'faq.html':
 		continue
 	if filename.endswith(".html"):
+		f = open(PAGES_DIR + "/" + filename, 'rt')
+		content = f.read()
+		f.close
+		if content.find("meta name=\"keywords\"") == -1:
+			continue
 		date_from_git = subprocess.run(["git", "log", "--reverse", "--date=iso", "--format=%cd", "--", filename], \
 		cwd=PAGES_DIR, \
 		stdout=subprocess.PIPE)
 		full_date = date_from_git.stdout.decode('utf-8')
 		date = full_date.split(' ')[0]
-		f = open(PAGES_DIR + "/" + filename, 'rt')
-		content = f.read()
-		f.close
 		title = content.split("<title>")[1].split("</title>")[0]
 		description = content.split('<meta name="description" content="')[1].split('">')[0]
 		keywords = content.split('<meta name="keywords" content="')[1].split('">')[0].split(', ')
