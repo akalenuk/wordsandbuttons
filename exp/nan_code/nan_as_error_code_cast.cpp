f7cbbb2485c76a77a2f7e170175b553da0362478
Oleksandr Kaleniuk <akalenuk@gmail.com>
2020-12-20 16:49:45 +0200

    same thing, no union

---
+++ exp/nan_code/nan_as_error_code_cast.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+enum class ECode : uint64_t {
+    INPUT_IS_NAN = 0xFFF0'0000'0000'0001,
+    INPUT_IS_INFINITE,
+    INPUT_IS_NEGATIVE
+};
+
+union Result_or_code
+{
+    double result;
+    Result_or_code(double x) {result = x;}
+    Result_or_code(ECode c) {result = *reinterpret_cast<double*>(&c);}
+    operator double() {return result;}
+    operator ECode() {return *reinterpret_cast<ECode*>(&result);}
+};
+
+Result_or_code sqrt_or_not(double x) {
+    if (std::isnan(x))
+        return ECode::INPUT_IS_NAN;
+    if (std::isinf(x))
+        return ECode::INPUT_IS_INFINITE;
+    if(x < 0)
+        return ECode::INPUT_IS_NEGATIVE;
+    return std::sqrt(x);
+}
+
+int main(void) {
+    assert(sqrt_or_not(-1.) == ECode::INPUT_IS_NEGATIVE);
+    assert(sqrt_or_not(1./0.) == ECode::INPUT_IS_INFINITE);
+    assert(sqrt_or_not(0./0.) == ECode::INPUT_IS_NAN);
+    assert(sqrt_or_not(4.) == 2.);
+
+    assert(std::isnan(sqrt_or_not(-1.)));
+    assert(std::isnan(sqrt_or_not(1./0.)));
+    assert(std::isnan(sqrt_or_not(0./0.)));
+    assert(std::isnan(sqrt_or_not(-1.) + 1.));
+}
