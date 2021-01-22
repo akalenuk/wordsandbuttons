789bf6d4091ffd2edcc885196ada8cb63d822808
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-01-22 20:01:49 +0200

    changing script

--- tools/change_things/change_mouse_to_pointer.py
+++ tools/change_things/change_mouse_to_pointer.py
@@ -4,22 +4,25 @@ path = '../../pages/'
 for file_name in os.listdir(path):
 	if os.path.isfile(path + file_name):
 		if file_name.endswith('.html'):
-			print (file_name)
 			html = open(path + file_name, 'r')
 			text = html.read()
 			html.close()
 			if text.find("<canvas") == -1:
 				continue
+			print (file_name)
 			lines = text.split('\n')
 			new_lines = []
 			for line in lines:
-				if line.strip() == '</style>':
-					new_lines += ["canvas { touch-action: none; }"]
-				line = line.replace(".addEventListener('mousemove')", ".addEventListener('pointermove')")
-				line = line.replace(".addEventListener('mousedown')", ".addEventListener('pointerdown')")
-				line = line.replace(".addEventListener('mouseup')", ".addEventListener('pointerup')")
-				line = line.replace(".addEventListener('mouseleave')", ".addEventListener('pointerleave')")
-				new_lines += [line]
+				if line.strip() == '</style>' and text.find("canvas { touch-action: none; }") == -1:
+					new_lines += ["", "canvas { touch-action: none; }"]
+				new_line = line
+				new_line = new_line.replace(".addEventListener('mousemove'", ".addEventListener('pointermove'")
+				new_line = new_line.replace(".addEventListener('mousedown'", ".addEventListener('pointerdown'")
+				new_line = new_line.replace(".addEventListener('mouseup'", ".addEventListener('pointerup'")
+				new_line = new_line.replace(".addEventListener('mouseleave'", ".addEventListener('pointerleave'")
+				new_lines += [new_line]
+				if new_line != line:
+					print(new_line)
 			new_text = '\n'.join(new_lines)
 			html = open(path + file_name, 'w')
 			html.write(new_text)
