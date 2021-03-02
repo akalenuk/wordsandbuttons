f0a09ae191cabfa97fa5aa686b3df26f143215fd
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-03-02 17:38:52 +0200

    script for changing the footer

--- tools/change_things/change_footer.py
+++ tools/change_things/change_footer.py
@@ -1,35 +1,28 @@
 import os
 
-new_footer = """<table class="footer" style="width: 555pt; padding: 64pt 0pt 32pt 0pt; background-color: transparent; font-family: sans-serif; font-size: 16pt; font-style: normal;">
-	<tr>
-	<td class="footer" style="vertical-align: middle; text-align: left; width: 64px; padding: 0; margin: 0; border: 0;">
-		<a href="index.html"><img src="favicon.svg"></a> 
-	</td>
-	<td class="footer" style="vertical-align: middle; text-align: left; width: 200pt; padding: 0; margin: 0; border: 0;">
-		&nbsp;&larr; there's more.
-	</td>
-	<td class="footer" style="vertical-align: middle; text-align: right; width: 300pt; padding: 0; margin: 0; border: 0;">
-		+
-		<a href="https://github.com/akalenuk/wordsandbuttons">Github</a> &
-		<a href="https://wordsandbuttons.online/rss"><span style="letter-spacing: 1pt;">RSS</span></a>
-	</td>
-	</tr>
-	</table>"""
+for_index = '<td class="footer" style="vertical-align: middle; text-align: left; width: 64px; padding: 0; margin: 0; border: 0;">'
+for_twitter = '<a href="https://github.com/akalenuk/wordsandbuttons">Github</a> &'
+
+with_index = '<td class="footer" style="vertical-align: middle; text-align: left; width: 64px; padding: 0; margin: 0; border: 0;">\n\
+		<a href="index.html">Index</a>'
+
+with_twitter = '<a href="https://github.com/akalenuk/wordsandbuttons">Github</a> &\n\
+		<a href="https://twitter.com/wordsandbuttons">Twitter</a> &'
 
 path = '../../pages/'
 for file_name in os.listdir(path):
 	if os.path.isfile(path + file_name):
 		if file_name.endswith('.html') and file_name != 'index.html':
-			print(file_name)
 			html = open(path + file_name, 'r')
 			text = html.read()
 			html.close()
-			before_and_with = text.split('<table class="footer"')
-			if len(before_and_with) != 2:
+
+			if text.find(for_index) == -1 or text.find(for_twitter) == -1:
 				continue
-			before = before_and_with[0]
-			after = before_and_with[1].split('</table>')[1]
-			new_text = before + new_footer + after
+			print(file_name)
+
+			new_text = text.replace(for_index, with_index).replace(for_twitter, with_twitter)
+
 			html = open(path + file_name, 'w')
 			html.write(new_text)
 			html.close()
