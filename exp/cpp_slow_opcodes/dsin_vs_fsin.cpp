afdfb012c4db81c4ac2497773d70f2be6e96b409
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-03-22 08:10:26 +0200

    std

--- exp/cpp_slow_opcodes/dsin_vs_fsin.cpp
+++ exp/cpp_slow_opcodes/dsin_vs_fsin.cpp
@@ -12,15 +12,15 @@ int main() {
     std::vector<float> fs(n);
     for (auto i = 0u; i < n; ++i) {
         const auto x = distribution(rng);
-		ds[i] = x;
-		fs[i] = static_cast<float>(x);
+        ds[i] = x;
+        fs[i] = static_cast<float>(x);
     }
 
 	if(true) {
 	    auto start = std::chrono::system_clock::now();
 	    double sum  = 0.;
 	    for (auto i = 0u; i < n; ++i)
-	        sum += sin(ds[i]);
+	        sum += std::sin(ds[i]);
 	    auto end = std::chrono::system_clock::now();
 	    std::cout << "double sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
 	}
@@ -28,7 +28,7 @@ int main() {
 	    auto start = std::chrono::system_clock::now();
 	    float sum  = 0.;
 	    for (auto i = 0u; i < n; ++i)
-	        sum += sin(fs[i]);
+	        sum += std::sin(fs[i]);
 	    auto end = std::chrono::system_clock::now();
 	    std::cout << "float sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
 	}
