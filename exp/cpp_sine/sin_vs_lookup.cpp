dd5334a71215499d15bc191368a344fb0038fc8c
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-03-25 20:41:05 +0200

    first round code

--- exp/cpp_sine/sin_vs_lookup.cpp
+++ exp/cpp_sine/sin_vs_lookup.cpp
@@ -13,26 +13,26 @@ int main() {
         x = distribution(rng);
     }
 
-	constexpr auto lookup_n = 100;
-	std::array<double, lookup_n> lookup;
-	for(auto i = 0u; i < lookup_n; ++i)
-		lookup[i] = sin(static_cast<double>(i) / static_cast<double>(lookup_n));
+    constexpr auto lookup_n = 100;
+    std::array<double, lookup_n> lookup;
+    for(auto i = 0u; i < lookup_n; ++i)
+        lookup[i] = sin(static_cast<double>(i) / static_cast<double>(lookup_n));
 
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    double sum  = 0.;
-	    for (auto i = 0u; i < n; ++i)
-	        sum += sin(xs[i]);
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
-	}
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    double sum  = 0.;
-	    for (auto i = 0u; i < n; ++i)
-	        sum += lookup[static_cast<unsigned int>(xs[i] * lookup_n)];
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "lookup time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
-	}
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        double sum  = 0.;
+        for (auto i = 0u; i < n; ++i)
+            sum += std::sin(xs[i]);
+        auto end = std::chrono::system_clock::now();
+        std::cout << "sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
+    }
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        double sum  = 0.;
+        for (auto i = 0u; i < n; ++i)
+            sum += lookup[static_cast<unsigned int>(xs[i] * lookup_n)];
+        auto end = std::chrono::system_clock::now();
+        std::cout << "lookup time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
+    }
 
 }
