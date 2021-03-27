8151df2a4aa9954f51ac66a15450a71c744d71b5
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-03-27 09:06:00 +0200

    round 4

--- exp/cpp_sine/fsin_vs_stdfsin.cpp
+++ exp/cpp_sine/fsin_vs_stdfsin.cpp
@@ -12,20 +12,20 @@ int main() {
     for (auto i = 0u; i < n; ++i)
         fs[i] = distribution(rng);
 
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    double sum  = 0.;
-	    for (auto i = 0u; i < n; ++i)
-	        sum += std::sin(fs[i]);
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "std::sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
-	}
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    float sum  = 0.;
-	    for (auto i = 0u; i < n; ++i)
-	        sum += sin(fs[i]);
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
-	}
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        double sum  = 0.;
+        for (auto i = 0u; i < n; ++i)
+            sum += std::sin(fs[i]);
+        auto end = std::chrono::system_clock::now();
+        std::cout << "std::sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
+    }
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        float sum  = 0.;
+        for (auto i = 0u; i < n; ++i)
+            sum += sin(fs[i]);
+        auto end = std::chrono::system_clock::now();
+        std::cout << "sin time: " << (end-start).count() * 1e-9  << "  sum: " << sum << "\n";
+    }
 }
