30cd591df6fa567800b343c5576a9178fe45a2ed
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-03-27 09:11:52 +0200

    round 6

--- exp/cpp_sine/div_vs_mul.cpp
+++ exp/cpp_sine/div_vs_mul.cpp
@@ -2,26 +2,26 @@
 #include <iostream>
 
 int main() {
-	const int n = 10000000;
-	volatile double a = 2.;
-	volatile double b = 0.5;
+    const int n = 10000000;
+    volatile double a = 2.;
+    volatile double b = 0.5;
 
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    double result = 1.;
-	    for (auto i = 0u; i < n; ++i)
-	        result = result * a * b;
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "mul time: " << (end-start).count() * 1e-9  << "  1: " << result << "\n";
-	}
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        double result = 1.;
+        for (auto i = 0u; i < n; ++i)
+            result = result * a * b;
+        auto end = std::chrono::system_clock::now();
+        std::cout << "mul time: " << (end-start).count() * 1e-9  << "  1: " << result << "\n";
+    }
 
-	if(true) {
-	    auto start = std::chrono::system_clock::now();
-	    double result = 1.;
-	    for (auto i = 0u; i < n; ++i)
-	        result = result / a / b;
-	    auto end = std::chrono::system_clock::now();
-	    std::cout << "div time: " << (end-start).count() * 1e-9  << "  1: " << result << "\n";
-	}
+    if(true) {
+        auto start = std::chrono::system_clock::now();
+        double result = 1.;
+        for (auto i = 0u; i < n; ++i)
+            result = result / a / b;
+        auto end = std::chrono::system_clock::now();
+        std::cout << "div time: " << (end-start).count() * 1e-9  << "  1: " << result << "\n";
+    }
 }
 
