9200349498eaccd89f0231f37273ea7304ec9177
Oleksandr Kaleniuk <akalenuk@gmail.com>
2021-04-17 14:43:45 +0300

    compiles

--- exp/cuda/vectorAdd.cu
+++ exp/cuda/vectorAdd.cu
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <random>
 #include <cuda_runtime.h>
 
 
@@ -9,15 +11,16 @@ __global__ void add(const float *A, const float *B, float *C, int numElements) {
 	}
 }
 
-#define attempt(smth) {auto s=(smth);if(s!=cudaSuccess){std::cout << cudaGetErrorString() << "\n"; return -1;}}
+#define attempt(smth) {auto s=(smth);if(s!=cudaSuccess){std::cout << cudaGetErrorString(s) << "\n"; return -1;}}
 
 int main(void)
 {
 	// prepare the data
 	using TheType = float;
 	constexpr auto TheSize = 65536;
+	constexpr auto TheSizeInBytes = TheSize*sizeof(TheType);
 	std::mt19937 rng(0);
-	std::uniform_real_distribution<float> distribution(0.f, 1.f);
+	std::uniform_real_distribution<TheType> distribution(0.f, 1.f);
 	std::vector<TheType> xs(TheSize);
 	std::vector<TheType> ys(TheSize);
 	std::vector<TheType> zs(TheSize);
@@ -29,24 +32,25 @@ int main(void)
 	float *d_xs = nullptr;
 	float *d_ys =  nullptr;
 	float *d_zs =  nullptr;
-	attempt(cudaMalloc((void **)&d_xs, size));
-	attempt(cudaMalloc((void **)&d_ys, size));
-	attempt(cudaMalloc((void **)&d_zs, size));
+	attempt(cudaMalloc((void **)&d_xs, TheSizeInBytes));
+	attempt(cudaMalloc((void **)&d_ys, TheSizeInBytes));
+	attempt(cudaMalloc((void **)&d_zs, TheSizeInBytes));
 
 	// and copying
-	attempt(cudaMemcpy(d_xs, xs, theSize*sizeof(float), cudaMemcpyHostToDevice));
-	attempt(cudaMemcpy(d_zs, zs, theSize*sizeof(float), cudaMemcpyHostToDevice));
+	attempt(cudaMemcpy(d_xs, xs.data(), TheSizeInBytes, cudaMemcpyHostToDevice));
+	attempt(cudaMemcpy(d_zs, zs.data(), TheSizeInBytes, cudaMemcpyHostToDevice));
 
 	// timestamp start
 	cudaEvent_t start;
 	cudaEventCreate(&start);
 	cudaEventRecord(start, 0);
+	cudaEvent_t stop;
 	cudaEventCreate(&stop); // here so it wouldn't interfere with the measurement
 
 	// run it
 	int threadsPerBlock = 256;
-	int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
-	add<<<blocksPerGrid, threadsPerBlock>>>(d_xs, d_ys, d_zx, theSize);
+	int blocksPerGrid = (TheSize + threadsPerBlock - 1) / threadsPerBlock;
+	add<<<blocksPerGrid, threadsPerBlock>>>(d_xs, d_ys, d_zs, TheSize);
 	attempt(cudaGetLastError());
 
 	// timestamp stop
@@ -57,11 +61,11 @@ int main(void)
 	std::cout << "Time: " << elapsedTime << "\n";
 
 	// back
-	attempt(cudaMemcpy(zs, d_zs, theSize*sizeof(float), cudaMemcpyDeviceToHost));
+	attempt(cudaMemcpy(zs.data(), d_zs, TheSizeInBytes, cudaMemcpyDeviceToHost));
 
 	// verification
-	for (auto i = 0u; i < the_size; ++i)
-		if (std::fabs(xs[i] + ys[i] - za[i]) > 1e-5)
+	for (auto i = 0u; i < TheSize; ++i) {
+		if (std::fabs(xs[i] + ys[i] - zs[i]) > 1.e-5) {
 			std::cout << "Not verified\n";
 			return -1;
 		}
