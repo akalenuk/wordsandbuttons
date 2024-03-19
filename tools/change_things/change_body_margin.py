12a7e59efcc40e6ba3c0439e5c2e25958beb6413
Oleksandr Kaleniuk <akalenuk@gmail.com>
2024-03-19 08:56:41 +0200

    body margin

---
+++ tools/change_things/change_body_margin.py
@@ -0,0 +1,25 @@
+import os
+
+def change_recursively(path):
+    for file_name in os.listdir(path):
+        if os.path.isfile(path + '/' + file_name):
+            if file_name.endswith('.html'):
+                print (file_name)
+                html = open(path + '/' + file_name, 'r')
+                text = html.read()
+                html.close()
+                new_text = text.replace('body {\n\tmargin: 0 0 0 0;', 'body {\n\tmargin: 12pt;')
+                new_text = new_text.replace('body {\r\n\tmargin: 0 0 0 0;', 'body {\r\n\tmargin: 12pt;')
+                new_text = new_text.replace('body {\n    margin: 0 0 0 0;', 'body {\n    margin: 12pt;')
+                new_text = new_text.replace('body {\r\n    margin: 0 0 0 0;', 'body {\r\n    margin: 12pt;')
+                new_text = new_text.replace('body{\n\tmargin: 0 0 0 0;', 'body {\n\tmargin: 12pt;')
+                new_text = new_text.replace('body{\r\n\tmargin: 0 0 0 0;', 'body {\r\n\tmargin: 12pt;')
+                new_text = new_text.replace('body{\n    margin: 0 0 0 0;', 'body {\n    margin: 12pt;')
+                new_text = new_text.replace('body{\r\n    margin: 0 0 0 0;', 'body {\r\n    margin: 12pt;')
+                html = open(path + '/' + file_name, 'w')
+                html.write(new_text)
+                html.close()
+        if os.path.isdir(path + '/' + file_name):
+            change_recursively(path + '/' + file_name)
+
+change_recursively('../..')
