2dc7a869a2bd25e65b7084d0b367b893c14be095
Oleksandr Kaleniuk <akalenuk@gmail.com>
2025-10-26 21:26:27 +0200

    New index

--- tools/generate_things/generate_navigation.py
+++ tools/generate_things/generate_navigation.py
@@ -13,15 +13,15 @@ keyword_note = {
 }
 
 index_title = 'Hello, world!'
-index_description = 'This is <i>Words and Buttons Online</i> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos<a/>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.'
+index_description = 'This is <i>Words and Buttons Online</i> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos</a>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.'
 
 keyword_description = {
-'tutorials':   'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <span style="color: #777;">#tutorials</span>, <a href="all_demos.html">#demos<a/>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
+'tutorials':   'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <span style="color: #777;">#tutorials</span>, <a href="all_demos.html">#demos</a>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
 'demos':       'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <span style="color: #777;">#demos</span>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
-'quizzes':     'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos<a/>, and <span style="color: #777;">#quizzes</span> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
-'mathematics': 'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos<a/>, and <a href="all_quizzes.html">#quizzes</a> about <span style="color: #777;">#mathematics</span>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
-'algorithms':  'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos<a/>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <span style="color: #777;">#algorithms</span>, and <a href="all_programming.html">#programming</a>.',
-'programming': 'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos<a/>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <span style="color: #777;">#programming</span>.'
+'quizzes':     'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos</a>, and <span style="color: #777;">#quizzes</span> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
+'mathematics': 'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos</a>, and <a href="all_quizzes.html">#quizzes</a> about <span style="color: #777;">#mathematics</span>, <a href="all_algorithms.html">#algorithms</a>, and <a href="all_programming.html">#programming</a>.',
+'algorithms':  'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos</a>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <span style="color: #777;">#algorithms</span>, and <a href="all_programming.html">#programming</a>.',
+'programming': 'This is <a href="index.html">Words and Buttons Online</a> — a collection of&nbsp;interactive <a href="all_tutorials.html">#tutorials</a>, <a href="all_demos.html">#demos</a>, and <a href="all_quizzes.html">#quizzes</a> about <a href="all_mathematics.html">#mathematics</a>, <a href="all_algorithms.html">#algorithms</a>, and <span style="color: #777;">#programming</span>.'
 }
 
 all_span_ids = []
@@ -92,11 +92,15 @@ index = '%s' % template
 f = open('links.txt')
 links = f.readlines()
 f.close()
-links_html = '<span id="links"><h1>More interactive learning</h1></span>'
+links_html = '<span id="links"><h1>More interactive learning\n'
+links_html += '<button id="unroll_links" type="button" onclick="unroll(\'links\')"><nobr>Unroll links</nobr></button>\n'
+links_html += '<button id="roll_links" type="button" onclick="roll(\'links\')" style="display:none"><nobr>Roll links</nobr></button>\n'
+links_html += '</h1></span>\n'
+links_html += '<span id="links_rollable">\n'
 for link in links:
 	if link.strip().find(' ') != -1:
 		url = link.split(' ')[0]
-		title_chunks = link.split(' ')[1:]
+		title_chunks = link.replace('\n', '').split(' ')[1:]
 		title = title_chunks[0]
 		for chunk in title_chunks[1:]: # no hanging short words
 			if len(chunk) < 2:
@@ -104,26 +108,36 @@ for link in links:
 			else:
 				title += ' ' + chunk
 		links_html += '<p style="margin-bottom: 12pt;">'+title+'<br><a href="'+url+'">'+url+'</a></p>\n'
-
+links_html += '</span>\n'
 
 # index is now real index not a timeline
-the_index = '<span id="index"><h1 title="A real index on index.html! How cool is that!">Index</h1></span>'
+the_index = '<span id="index"><h1 title="A real index on index.html! How cool is that!">Index\n'
+the_index += '<button id="unroll_index" type="button" onclick="unroll(\'index\')"><nobr>Unroll index</nobr></button>\n'
+the_index += '<button id="roll_index" type="button" onclick="roll(\'index\')" style="display:none"><nobr>Roll index</nobr></button>\n'
+the_index += '</h1></span>\n'
+the_index += '<span id="index_rollable">\n'
 spans = read_index_spans(PAGES_DIR)
 cur_letter = ''
 for (f, i, t) in sorted(spans, key = lambda fit: fit[2].upper()):
 	letter = t[0].upper()
+	if not letter.isalpha():
+		continue
 	if cur_letter != letter:
 		if cur_letter != '':
 			the_index += '</p>\n'
 		the_index += '<h2>'+letter+'</h2>\n'
-		the_index += '<p class="index_items">\n'
+		the_index += '<p class="index_items">'
 		cur_letter = letter
-	the_index += '<nobr><a style="padding-right: 24pt;" href="' + f + '#' + i + '">' + t + '</a></nobr>\n'
+	the_index += '<nobr><a style="padding-right: 24pt;" href="' + f + '#' + i + '">' + t + '</a></nobr> '
 the_index += '</p>\n'
-
+the_index += '</span>\n'
 
 # here is the timeline too
-timeline = '<span id="timeline"><h1>All the pages</h1></span>'
+timeline = '<span id="timeline"><h1>All the pages\n'
+timeline += '<button id="unroll_pages" type="button" onclick="unroll(\'pages\')"><nobr>Unroll pages</nobr></button>\n'
+timeline += '<button id="roll_pages" type="button" onclick="roll(\'pages\')" style="display:none"><nobr>Roll pages</nobr></button>\n'
+timeline += '</h1></span>\n'
+timeline += '<span id="pages_rollable">\n'
 n = 0
 total = len(date_link_title_description_keywords)
 for (d, l, t, desc, kwds) in date_link_title_description_keywords[::-1]:
@@ -134,7 +148,7 @@ for (d, l, t, desc, kwds) in date_link_title_description_keywords[::-1]:
         timeline += '<a style="padding-right: 8pt;" href="all_' + kw + '.html">#' + kw + '</a> '
     timeline += '</p>\n'
     n += 1
-
+timeline += '</span>\n'
 
 index = index.replace('<h1>Title</h1>', '<h1>' + index_title + '</h1>')
 index = index.replace('<p>Description</p>', '<p style="width: 600pt;">' + index_description + '</p>')
